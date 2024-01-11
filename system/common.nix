d21c478d996cc8996033538d732b4eaa8c1cdbe6
Josh <josh@jabberwhiskey.com>
2024-01-11 02:21:53 -0700

    added a common nix config all configs can pull from

---
+++ system/common.nix
@@ -0,0 +1,9 @@
+#A common config needed for all (my) nixos systems
+{ pkgs, config, ... }:
+{
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  time.timeZone = "US/Mountain";
+
+}
