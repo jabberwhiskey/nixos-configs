0ceb071f9c3f9daa8667a77cf8387cfca2262176
Josh <josh@jabberwhiskey.com>
2024-03-18 18:46:51 -0600

    fixed up sdr.nix

--- system/sdr.nix
+++ system/sdr.nix
@@ -1,4 +1,4 @@
-{config, pkgs}:
+{config, pkgs, ...}:
 {
   boot.kernelParams = [ "modprobe.blacklist=dvb_usb_rtl28xxu" ]; # blacklist this module
   services.udev.packages = [ pkgs.rtl-sdr ]; # (there might be other packages that require udev here too)
