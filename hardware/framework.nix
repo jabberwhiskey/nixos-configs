ed2b5b2a007e457268e71a032400b72be54dd634
jabberwhiskey <109061274+jabberwhiskey@users.noreply.github.com>
2024-03-20 01:27:32 +0000

    Update framework.nix

--- hardware/framework.nix
+++ hardware/framework.nix
@@ -35,7 +35,7 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp170s0.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux"
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
