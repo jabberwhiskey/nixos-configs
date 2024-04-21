36c62ad5f2c4484931df123f55805e545f69ff1f
jcw <jwillbanks2@gmail.com>
2024-04-21 07:56:17 -0600

    added steam tinkerlaunch

--- system/steam.nix
+++ system/steam.nix
@@ -1,13 +1,17 @@
-{ config, pkgs, ...  }:
+{ config, pkgs, lib, ...  }:
 {
   programs.steam = {
     enable = true;
     gamescopeSession = {
       enable = true;
     };
+    extraCompatPackages = with pkgs; [
+      steamtinkerlaunch
+    ];
   }; 
   environment.systemPackages = [
     pkgs.lutris
     pkgs.gamescope
+    pkgs.steamtinkerlaunch
   ];
 }
