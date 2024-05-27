0f792ee6f042a55ac5bdaece47c417c8aa6cc83e
jcw <jwillbanks2@gmail.com>
2024-05-27 05:54:27 -0600

    added idle inhibit

--- flake.nix
+++ flake.nix
@@ -125,7 +125,7 @@
                     sink_whitelist = [
                     ];
                   };
-		};`
+		};
                 imports = [
                   ./home/home.nix
 		  ./home/hyprland.nix
