d5e269d9113d80699e421946f6f2edce3e4a4ef0
jcw <jwillbanks2@gmail.com>
2024-06-13 01:56:46 -0600

    udated lockfile, changed swayidle

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -181,30 +181,30 @@
           	  --grace 2 \
           	  --fade-in 0.2'';
       }
-      {
-        timeout = 600;
-        command = "${pkgs.systemd}/bin/systemctl suspend";
-      }
+#      {
+#        timeout = 600;
+#        command = "${pkgs.systemd}/bin/systemctl suspend";
+#      }
     ];
-    events = [
-      {
-        event = "before-sleep";
-        command = ''          ${pkgs.swaylock-effects}/bin/swaylock --screenshots \
-                    --clock \
-          	  --indicator \
-          	  --indicator-radius 100 \
-          	  --indicator-thickness 7 \
-          	  --effect-blur 7x5 \
-          	  --effect-vignette 0.5:0.5 \
-          	  --ring-color bb00cc \
-          	  --key-hl-color 880033 \
-          	  --line-color 00000000 \
-          	  --inside-color 00000088 \
-          	  --separator-color 00000000 \
-          	  --grace 2 \
-          	  --fade-in 0.2'';
-      }
-    ];
+#    events = [
+#      {
+#        event = "before-sleep";
+#        command = ''          ${pkgs.swaylock-effects}/bin/swaylock --screenshots \
+#                    --clock \
+#          	  --indicator \
+#          	  --indicator-radius 100 \
+#          	  --indicator-thickness 7 \
+#          	  --effect-blur 7x5 \
+#          	  --effect-vignette 0.5:0.5 \
+#          	  --ring-color bb00cc \
+#          	  --key-hl-color 880033 \
+#          	  --line-color 00000000 \
+#          	  --inside-color 00000088 \
+#          	  --separator-color 00000000 \
+#          	  --grace 2 \
+#          	  --fade-in 0.2'';
+#      }
+#    ];
   };
   programs.wpaperd = {
     enable = true;
