b92d41347ac4cdc701a18e5e822b7a23faddd4ce
jcw <jwillbanks2@gmail.com>
2024-08-16 06:25:11 -0600

    deisbaled kernel-latest

--- hosts/nixos-laptop.nix
+++ hosts/nixos-laptop.nix
@@ -14,8 +14,8 @@
 #    ../system/qt.nix
     ../system/waydroid.nix
     ../system/greetd.nix
-    ../system/kernel-latest.nix
-    #    ../system/plasma.nix
+#    ../system/kernel-latest.nix
+#    ../system/plasma.nix
     ../system/fonts.nix
     ../system/systemdboot.nix
     ../system/containers.nix
