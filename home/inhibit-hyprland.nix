ded13c6eda62cccc418becf57f151e9d3291cbe1
jcw <jwillbanks2@gmail.com>
2024-08-18 08:51:19 -0600

    changed font packages to stabel branch, removed piepwire idle inhibit flake and switched to nixpkgs version

--- home/inhibit-hyprland.nix
+++ home/inhibit-hyprland.nix
@@ -1,15 +1,42 @@
-{wayland-pipewire-idle-inhibit, inputs, ...}:
+{config, pkgs, inputs, ...}:
 {
-  services.wayland-pipewire-idle-inhibit = {
+ home.packages = [ pkgs.wayland-pipewire-idle-inhibit ];
+#  services.wayland-pipewire-idle-inhibit = {
+#    enable = true;
+#    systemdTarget = "hyprland-session.target";
+#    settings = {
+#      verbosity = "INFO";
+#      media_minimum_duration = 10;
+#      idle_inhibitor = "wayland";
+#      sink_whitelist = [
+#      ];
+#    };
+#  };
+  home.file."${config.xdg.configHome}/wayland-pipewire-idle-inhibit/config.toml" = {
+    text = ''
+      verbosity = "WARN"
+      media_minimum_duration = 5
+      idle_inhibitor = "wayland"
+      sink_whitelist = [ ]
+      node_blacklist = [ ]
+    '';
     enable = true;
-    systemdTarget = "hyprland-session.target";
-    settings = {
-      verbosity = "INFO";
-      media_minimum_duration = 10;
-      idle_inhibitor = "wayland";
-      sink_whitelist = [
-      ];
-    };
   };
+  systemd.user.services = {
+    wayland-pipewire-idle-inhibit = {
+      Install = {
+        WantedBy = [ "hyprland-session.target" ];
+      };
+      Unit = {
+        Description = "dbus audio idle inhibitor";
+	Documentation = ["https://github.com/rafaelrc7/wayland-pipewire-idle-inhibit"];
+      };
+      Service = {
+	ExecStart = "${pkgs.wayland-pipewire-idle-inhibit}/bin/wayland-pipewire-idle-inhibit";
+	Restart = "Always";
+	RestartSec = 10;
+      };
+    };
+  };  
 }
 
--- system/fonts.nix
+++ system/fonts.nix
@@ -1,5 +1,10 @@
-{pkgs, ...}: {
-  fonts.packages = with pkgs; [
+{pkgs, inputs, config, pkgsStable,  ...}: {
+   # fonts fail to etch sometimes
+  _module.args.pkgsStable = import inputs.stable {
+    inherit (pkgs.stdenv.hostPlatform) system;
+    inherit (config.nixpkgs) config;
+  };
+  fonts.packages = with pkgsStable; [
     #    noto-fonts
     font-awesome
     fira-code
