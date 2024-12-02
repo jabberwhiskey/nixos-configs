53b5c2f75e92f4881efaf206e811b90e90c7f3f4
jcw <jwillbanks2@gmail.com>
2024-12-02 00:50:14 -0700

    added a bunch of stuff to steam to maybe play in gamescope

--- system/steam.nix
+++ system/steam.nix
@@ -7,11 +7,31 @@
     };
     extraCompatPackages = with pkgs; [
       steamtinkerlaunch
+      proton-ge-bin
     ];
+    package = pkgs.steam.override {
+      extraLibraries = pkgs: [ pkgs.xorg.libxcb ];
+      extraPkgs =
+        pkgs: with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+	  libkrb5
+          keyutils
+          gamemode
+        ];
+    };
   }; 
+  programs.gamemode.enable = true;
   environment.systemPackages = [
     pkgs.lutris
     pkgs.gamescope
     pkgs.steamtinkerlaunch
   ];
+
 }
