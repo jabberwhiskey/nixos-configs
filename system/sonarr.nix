38f926265d8488f7a943d126e603a0b4dfc088b3
jcw <jwillbanks2@gmail.com>
2024-12-16 20:47:00 -0700

    changed stuff

--- system/sonarr.nix
+++ system/sonarr.nix
@@ -1,5 +1,5 @@
 {pkgs, inputs, config, ... }:
-let 
+{
   nixpkgs.config.permittedInsecurePackages = [
     "aspnetcore-runtime-6.0.36"
     "aspnetcore-runtime-wrapped-6.0.36"
@@ -8,7 +8,6 @@ let
     "dotnet-sdk-6.0.428"
     "dotnet-sdk-wrapped-6.0.428"
   ];
-in {
   services.sonarr = {
     enable = true;
 #    package = inputs.nixpkgs.legacyPackages.x86_64-linux.sonarr;
