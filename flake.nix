c6146ddee9b381e0c9a78e2b7cc3f81bcc9ec038
jcw <jwillbanks2@gmail.com>
2025-02-21 03:12:26 -0700

    maybe fixed home-manager config

--- flake.nix
+++ flake.nix
@@ -96,8 +96,6 @@
       "jcw@craptop" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         extraSpecialArgs = {inherit inputs self;};
-        username = "jcw";
-        system = "x86_64-linux";
         modules = [
           ./hosts/craptop
         ];
