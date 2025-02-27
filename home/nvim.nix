10022c8e6e4c745808eb234c58b54014c5e3357e
jcw <jwillbanks2@gmail.com>
2025-02-26 19:54:33 -0700

    added some nvim keybinds

--- home/nvim.nix
+++ home/nvim.nix
@@ -1,8 +1,16 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 {
 xdg.configFile.nvim = {
   enable = true;
   recursive = true;
   source = ../extra/nvim;
+  };
+  programs.neovim = {
+    enable = true;
+    plugins = [
+      pkgs.vimPlugins.telescope-nvim
+      pkgs.vimPlugins.plenary-nvim
+      pkgs.vimPlugins.nvim-treesitter
+    ];
   };
 }
