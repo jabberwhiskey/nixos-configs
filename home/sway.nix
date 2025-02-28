c04eec9b5f036971b41a703336fa4baef43a6292
jcw <github.u9l99@aleeas.com>
2025-02-28 06:35:47 -0700

    removed swaylock effects

--- home/sway.nix
+++ home/sway.nix
@@ -1,170 +1,169 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}: {
-  wayland.windowManager.sway = {
-    enable = true;
-    checkConfig = false;
-    config = rec {
-      modifier = "Mod4";
-      left = "h";
-      down = "j";
-      up = "k";
-      right = "l";
-      menu = "${pkgs.rofi-wayland}/bin/rofi";
+  {
+    pkgs,
+    config,
+    lib,
+    ...
+  }: {
+    wayland.windowManager.sway = {
+      enable = true;
+      checkConfig = false;
+      config = rec {
+        modifier = "Mod4";
+        left = "h";
+        down = "j";
+        up = "k";
+        right = "l";
+        menu = "${pkgs.rofi-wayland}/bin/rofi";
 
-      terminal = "foot";
+        terminal = "foot";
 
-      workspaceAutoBackAndForth = true;
-      output = {
-        "*" = {
-          bg =  "${config.home.homeDirectory}/Pictures/Wallpapers/1382343.jpg fill";
-	};
-      };
-      window = {
-        titlebar = true;
-        hideEdgeBorders = "both";
-        commands = [
+        workspaceAutoBackAndForth = true;
+        output = {
+          "*" = {
+            bg =  "${config.home.homeDirectory}/Pictures/Wallpapers/1382343.jpg fill";
+    };
+        };
+        window = {
+          titlebar = true;
+          hideEdgeBorders = "both";
+          commands = [
+            {
+              command = "inhibit_idle fullscreen";
+              criteria.class = "mpv";
+            }
+          ];
+        };
+
+        bars = [
           {
-            command = "inhibit_idle fullscreen";
-            criteria.class = "mpv";
+            statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-default.toml";
+            position = "top";
+            command = "${pkgs.sway}/bin/swaybar";
+            id = "swaybar";
+            trayPadding = 5;
+            fonts = {
+              names = ["DejaVu Sans Mono" "FontAwesome5Free"];
+              style = "Bold Semi-Condensed";
+              size = 10.0;
+            };
           }
         ];
-      };
 
-      bars = [
-        {
-          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-default.toml";
-          position = "top";
-          command = "${pkgs.sway}/bin/swaybar";
-          id = "swaybar";
-          trayPadding = 5;
-          fonts = {
-            names = ["DejaVu Sans Mono" "FontAwesome5Free"];
-            style = "Bold Semi-Condensed";
-            size = 10.0;
-          };
-        }
-      ];
-
-      startup = [
-        {command = "${pkgs.dunst}/bin/dunst";}
+        startup = [
+          {command = "${pkgs.dunst}/bin/dunst";}
 #        {command = "nixGL ${pkgs.wpaperd}/bin/wpaperd";}
-      ];
-    };
-    config = {
-      keybindings = let
-        mod = config.wayland.windowManager.sway.config.modifier;
-        inherit
-          (config.wayland.windowManager.sway.config)
-          left
-          down
-          up
-          right
-          menu
-          terminal
-          ;
-      in {
-        "${mod}+Alt+w" = "exec ${pkgs.firefox}/bin/firefox";
-        "${mod}+Return" = "exec ${terminal}";
-        "${mod}+d" = "exec ${menu}  -show drun";
-
-        "${mod}+c" = "kill";
-        "${mod}+${left}" = "focus left";
-        "${mod}+${down}" = "focus down";
-        "${mod}+${up}" = "focus up";
-        "${mod}+${right}" = "focus right";
-
-        "${mod}+Left" = "focus left";
-        "${mod}+Down" = "focus down";
-        "${mod}+Up" = "focus up";
-        "${mod}+Right" = "focus right";
-
-        "${mod}+Shift+${left}" = "move left";
-        "${mod}+Shift+${down}" = "move down";
-        "${mod}+Shift+${up}" = "move up";
-        "${mod}+Shift+${right}" = "move right";
-
-        "${mod}+Shift+Left" = "move left";
-        "${mod}+Shift+Down" = "move down";
-        "${mod}+Shift+Up" = "move up";
-        "${mod}+Shift+Right" = "move right";
-
-        "${mod}+Shift+space" = "floating toggle";
-        "${mod}+space" = "focus mode_toggle";
-        "${mod}+b" = "splith";
-        "${mod}+v" = "splitv";
-
-        "${mod}+1" = "workspace number 1";
-        "${mod}+2" = "workspace number 2";
-        "${mod}+3" = "workspace number 3";
-        "${mod}+4" = "workspace number 4";
-        "${mod}+5" = "workspace number 5";
-        "${mod}+6" = "workspace number 6";
-        "${mod}+7" = "workspace number 7";
-        "${mod}+8" = "workspace number 8";
-        "${mod}+9" = "workspace number 9";
-        "${mod}+0" = "workspace number 10";
-
-        "${mod}+Shift+1" = "move container to workspace number 1";
-        "${mod}+Shift+2" = "move container to workspace number 2";
-        "${mod}+Shift+3" = "move container to workspace number 3";
-        "${mod}+Shift+4" = "move container to workspace number 4";
-        "${mod}+Shift+5" = "move container to workspace number 5";
-        "${mod}+Shift+6" = "move container to workspace number 6";
-        "${mod}+Shift+7" = "move container to workspace number 7";
-        "${mod}+Shift+8" = "move container to workspace number 8";
-        "${mod}+Shift+9" = "move container to workspace number 9";
-        "${mod}+Shift+0" = "move container to workspace number 10";
-
-        "${mod}+f" = "fullscreen toggle";
-        "${mod}+comma" = "layout stacking";
-        "${mod}+period" = "layout tabbed";
-        "${mod}+slash" = "layout toggle split";
-        "${mod}+a" = "focus parent";
-        "${mod}+s" = "focus child";
-
-        "${mod}+Shift+c" = "reload";
-        "${mod}+Shift+e" = "exec swaynag -t warning -m 'Do you want to exit Sway?' -b 'Yes, exit sway' 'swaymsg exit'";
-        "${mod}+r" = "mode resize";
-
-        "print" = "exec  ${pkgs.grim}/bin/grim -g- screenshot-$(date +%Y%m%d-%H%M%S).png";
-        "${mod}+print" = "exec ${pkgs.slurp}/bin/slurp | ${pkgs.grim}/bin/grim -g-  screenshot-$(date +%Y%m%d-%H%M%S).png";
-
-        "XF86AudioMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
-        "XF86AudioRaiseVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
-        "XF86AudioLowerVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%";
-
-        "XF86MonBrightnessUp" = "exec ${pkgs.light}/bin/light -A 10";
-        "XF86MonBrightnessDown" = "exec ${pkgs.light}/bin/light -U 10";
-
-        "${mod}+apostrophe" = "move workspace to output right";
-
-        "${mod}+minus" = "scratchpad show";
-        "${mod}+underscore" = "move container to scratchpad";
+        ];
       };
-      floating.criteria = [
-        {class = "Bitwarden";}
-      ];
+      config = {
+        keybindings = let
+          mod = config.wayland.windowManager.sway.config.modifier;
+          inherit
+            (config.wayland.windowManager.sway.config)
+            left
+            down
+            up
+            right
+            menu
+            terminal
+            ;
+        in {
+          "${mod}+Return" = "exec ${terminal}";
+          "${mod}+d" = "exec ${menu}  -show drun";
 
-      gaps = {
-        horizontal = 5;
-        vertical = 5;
-        inner = 5;
-        outer = 5;
-        smartBorders = "on";
-        smartGaps = true;
+          "${mod}+c" = "kill";
+          "${mod}+${left}" = "focus left";
+          "${mod}+${down}" = "focus down";
+          "${mod}+${up}" = "focus up";
+          "${mod}+${right}" = "focus right";
+
+          "${mod}+Left" = "focus left";
+          "${mod}+Down" = "focus down";
+          "${mod}+Up" = "focus up";
+          "${mod}+Right" = "focus right";
+
+          "${mod}+Shift+${left}" = "move left";
+          "${mod}+Shift+${down}" = "move down";
+          "${mod}+Shift+${up}" = "move up";
+          "${mod}+Shift+${right}" = "move right";
+
+          "${mod}+Shift+Left" = "move left";
+          "${mod}+Shift+Down" = "move down";
+          "${mod}+Shift+Up" = "move up";
+          "${mod}+Shift+Right" = "move right";
+
+          "${mod}+Shift+space" = "floating toggle";
+          "${mod}+space" = "focus mode_toggle";
+          "${mod}+b" = "splith";
+          "${mod}+v" = "splitv";
+
+          "${mod}+1" = "workspace number 1";
+          "${mod}+2" = "workspace number 2";
+          "${mod}+3" = "workspace number 3";
+          "${mod}+4" = "workspace number 4";
+          "${mod}+5" = "workspace number 5";
+          "${mod}+6" = "workspace number 6";
+          "${mod}+7" = "workspace number 7";
+          "${mod}+8" = "workspace number 8";
+          "${mod}+9" = "workspace number 9";
+          "${mod}+0" = "workspace number 10";
+
+          "${mod}+Shift+1" = "move container to workspace number 1";
+          "${mod}+Shift+2" = "move container to workspace number 2";
+          "${mod}+Shift+3" = "move container to workspace number 3";
+          "${mod}+Shift+4" = "move container to workspace number 4";
+          "${mod}+Shift+5" = "move container to workspace number 5";
+          "${mod}+Shift+6" = "move container to workspace number 6";
+          "${mod}+Shift+7" = "move container to workspace number 7";
+          "${mod}+Shift+8" = "move container to workspace number 8";
+          "${mod}+Shift+9" = "move container to workspace number 9";
+          "${mod}+Shift+0" = "move container to workspace number 10";
+
+          "${mod}+f" = "fullscreen toggle";
+          "${mod}+comma" = "layout stacking";
+          "${mod}+period" = "layout tabbed";
+          "${mod}+slash" = "layout toggle split";
+          "${mod}+a" = "focus parent";
+          "${mod}+s" = "focus child";
+
+          "${mod}+Shift+c" = "reload";
+          "${mod}+Shift+e" = "exec swaynag -t warning -m 'Do you want to exit Sway?' -b 'Yes, exit sway' 'swaymsg exit'";
+          "${mod}+r" = "mode resize";
+
+          "print" = "exec  ${pkgs.grim}/bin/grim -g- screenshot-$(date +%Y%m%d-%H%M%S).png";
+          "${mod}+print" = "exec ${pkgs.slurp}/bin/slurp | ${pkgs.grim}/bin/grim -g-  screenshot-$(date +%Y%m%d-%H%M%S).png";
+
+          "XF86AudioMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
+          "XF86AudioRaiseVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
+          "XF86AudioLowerVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%";
+
+          "XF86MonBrightnessUp" = "exec ${pkgs.light}/bin/light -A 10";
+          "XF86MonBrightnessDown" = "exec ${pkgs.light}/bin/light -U 10";
+
+          "${mod}+apostrophe" = "move workspace to output right";
+
+          "${mod}+minus" = "scratchpad show";
+          "${mod}+underscore" = "move container to scratchpad";
+        };
+        floating.criteria = [
+          {class = "Bitwarden";}
+        ];
+
+        gaps = {
+          horizontal = 5;
+          vertical = 5;
+          inner = 5;
+          outer = 5;
+          smartBorders = "on";
+          smartGaps = true;
+        };
+      };
+      systemd.enable = true;
+      extraConfig = ''
+      '';
+      swaynag = {
+        enable = true;
       };
     };
-    systemd.enable = true;
-    extraConfig = ''
-    '';
-    swaynag = {
-      enable = true;
-    };
-  };
 #  programs.wofi = {
 #    enable = true;
 #    style = ''
@@ -177,11 +176,10 @@
 #      allow_images = true;
 #    };
 #  };
-  programs.i3status-rust = {
-    enable = true;
-    bars = {
-      default = {
-        "${mod}+Alt+b" = "exec bitwarden";
+    programs.i3status-rust = {
+      enable = true;
+      bars = {
+        default = {
         blocks = [
           {
             block = "battery";
@@ -240,41 +238,13 @@
  
       {
         timeout = 300;
-        command = ''          
-	         ${pkgs.swaylock-effects}/bin/swaylock  --screenshots \
-          	 --clock \
-          	 --indicator \
-          	 --indicator-radius 100 \
-          	 --indicator-thickness 7 \
-          	 --effect-blur 7x5 \
-          	 --effect-vignette 0.5:0.5 \
-          	 --ring-color bb00cc \
-          	 --key-hl-color 880033 \
-          	 --line-color 00000000 \
-          	 --inside-color 00000088 \
-          	 --separator-color 00000000 \
-          	 --grace 2 \
-          	 --fade-in 0.2'';
+        command = "/bin/swaylock";      
       }
     ];
     events = [
       {
         event = "before-sleep";
-        command = ''
-	         ${pkgs.swaylock-effects}/bin/swaylock --screenshots \
-                    --clock \
-          	 --indicator \
-          	 --indicator-radius 100 \
-          	 --indicator-thickness 7 \
-          	 --effect-blur 7x5 \
-          	 --effect-vignette 0.5:0.5 \
-          	 --ring-color bb00cc \
-          	 --key-hl-color 880033 \
-          	 --line-color 00000000 \
-          	 --inside-color 00000088 \
-          	 --separator-color 00000000 \
-          	 --grace 2 \
-          	 --fade-in 0.2'';
+        command = "/bin/swaylock";
       }
     ];
   };
