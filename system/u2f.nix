ecf8dcad04d88d8fc4212221ba296edb678f0246
jcw <jwillbanks2@gmail.com>
2025-03-02 19:45:03 -0700

    added security key stuff

--- system/u2f.nix
+++ system/u2f.nix
@@ -14,21 +14,14 @@
  # https://github.com/solokeys/solo2-cli/blob/main/70-solo2.rules
  services.udev.packages = [
      pkgs.yubikey-personalization
-     (pkgs.writeTextFile {
-     name = "wally_udev";
-     text ='' 
-         # NXP LPC55 ROM bootloader (unmodified)
-         SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1fc9", ATTRS{idProduct}=="0021", TAG+="uaccess"
-         # NXP LPC55 ROM bootloader (with Solo 2 VID:PID)
-         SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="b000", TAG+="uaccess"
-         # Solo 2
-         SUBSYSTEM=="tty", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="beee", TAG+="uaccess"
-         # Solo 2
-         SUBSYSTEM=="usb", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="beee", TAG+="uaccess"
-     '';
-     destination = "/etc/udev/rules.d/70-solo2.rules";
-     })
  ];
- }
+ security.pam.yubico = {
+   enable = true;
+   debug = true;
+   mode = "challenge-response";
+  # id = [ "12345678" ];
+};
+services.pcscd.enable = true;
+}
 
 
