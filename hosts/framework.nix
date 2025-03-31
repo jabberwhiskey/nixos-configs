c80c267b93efdb088904c21b9be5aab714d37144
jcw <github.u9l99@aleeas.com>
2025-03-31 03:43:58 -0600

    fixed framework.nix

--- hosts/framework.nix
+++ hosts/framework.nix
@@ -70,7 +70,6 @@
       package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
       portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
-  services
   hardware.brillo.enable = true;
   hardware.bluetooth.enable = true; # enables support for Bluetooth
     hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
