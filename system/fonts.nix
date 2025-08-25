1d70edcda4578c7dd1dc9f6c06ae89f7a345db6e
jcw <github.u9l99@aleeas.com>
2025-08-25 05:30:54 -0600

    cleaned up fontconfig

--- system/fonts.nix
+++ system/fonts.nix
@@ -1,12 +1,20 @@
 {pkgs, inputs, config, ...}: {
-  fonts.packages = with inputs.stable.legacyPackages.x86_64-linux; [
-    #    noto-fonts
-    font-awesome
-    fira-code-symbols
-    dina-font
-    nerd-fonts.noto
-    nerd-fonts.ubuntu
-    nerd-fonts._0xproto
-    nerd-fonts.fira-code
-  ];
+  fonts = { 
+    packages = with inputs.stable.legacyPackages.x86_64-linux; [
+      #    noto-fonts
+      font-awesome
+      fira-code-symbols
+      dina-font
+      nerd-fonts.noto
+      nerd-fonts.ubuntu
+      nerd-fonts._0xproto
+      nerd-fonts.fira-code
+      ubuntu-sans
+      ubuntu-classic
+      ubuntu-sans-mono
+    ];
+    fontconfig = {
+      };
+    };
+  };
 }
