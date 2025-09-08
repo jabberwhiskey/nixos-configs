6d45c5ed00ad0a632b45ba6d6463b49d779d6443
jcw <github.u9l99@aleeas.com>
2025-09-08 08:57:14 -0600

    fixed anyrun cuz config got broken

--- home/anyrun.nix
+++ home/anyrun.nix
@@ -40,34 +40,90 @@
         // for any other plugin
         // this file will be put in ~/.config/anyrun/some-plugin.ron
         // refer to docs of xdg.configFile for available options
-        engines: [google]
+        engines: [google],
+        max_entries: 10,
       )
     '';
     extraConfigFiles."applications.ron".text = ''
-      config(
+      Config(
         desktop_actions: true,
         terminal: Some(Terminal(
         command: "foot",
-        args: "-e {}",
+        args: "-e {}")),  
+        max_entries: 10,
       )
+
     '';
     # Inline comments are supported for language injection into
     # multi-line strings with Treesitter! (Depends on your editor)
     extraCss = # css
       ''
         * {
-          all: unset;
-          font-family: JetBrainsMono Nerd Font;
 
         }
-        entry#entry {
-          background: red;
+        box.main {
+          background-color: black;
+          padding: 5px;
+          margin: 10px;
+          border-radius: 10px;
+          border: 2px solid @theme_selected_bg_color;
+          box-shadow: 0 0 5px black;  
         }
-        #window {
-          Background  : transparent; 
+        window {
+          background  : transparent; 
+        }
+        text {
+          min-height: 30px;
+          padding: 5px;
+          border-radius: 5px;
+        }
+        
+        .matches {
+          background-color: rgba(0, 0, 0, 0);
+          border-radius: 10px;
+        }
+        
+        box.plugin:first-child {
+          margin-top: 5px;
+        }
+        
+        box.plugin.info {
+          min-width: 200px;
+        }
+        
+        list.plugin {
+          background-color: rgba(0, 0, 0, 0);
+        }
+        
+        label.match.description {
+          font-size: 10px;
+        }
+        
+        label.plugin.info {
+          font-size: 14px;
+        }
+        
+        .match {
+          background: transparent;
+        }
+        
+        .match:selected {
+          border-left: 4px solid @theme_selected_bg_color;
+          background: transparent;
+          animation: fade 0.1s linear;
+        }
+        
+        @keyframes fade {
+          0% {
+            opacity: 0;
+          }
+        
+          100% {
+            opacity: 1;
+          }
         }
       '';
 
   };
-  wayland.windowManager.hyprland.settings.bind = [ "$mod, R, exec, ${pkgs.anyrun}/bin/anyrun" ];
+  wayland.windowManager.hyprland.settings.bind = [ "$mod, R, exec, anyrun" ];
 }
