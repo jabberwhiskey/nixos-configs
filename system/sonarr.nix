cac31889ff896f1cbeb392ac794a011cab9deca4
jcw <github.u9l99@aleeas.com>
2025-11-21 01:21:58 -0700

    changed perms for sonarr

--- system/sonarr.nix
+++ system/sonarr.nix
@@ -16,7 +16,8 @@
   services.sonarr = {
     enable = true;
     #    package = inputs.nixpkgs.legacyPackages.x86_64-linux.sonarr;
-    openFirewall = true;
-    user = "jcw";
+#    openFirewall = true;
+    user = "sonarr";
+    group = "users";
   };
 }
