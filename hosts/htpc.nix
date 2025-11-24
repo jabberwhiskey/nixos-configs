12a6c664258ec9fc0ab5f4a4459d611668aa4e36
jcw <jwillbanks2@gmail.com>
2025-11-24 07:40:46 -0700

    added sound to htpc

--- hosts/htpc.nix
+++ hosts/htpc.nix
@@ -25,4 +25,14 @@
   nixpkgs.hostPlatform = "x86_64-linux";
   system.stateVersion = "25.05";
 
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = true;
+  };
+  networking.firewall.enable = true;
+
 }
