b7887f200c4bdbdb3c36f0691d7beb921482429b
jcw <jwillbanks2@gmail.com>
2025-11-26 03:57:57 -0700

    fixed transmission incomplete-directory

--- system/server-torrent.nix
+++ system/server-torrent.nix
@@ -8,6 +8,7 @@
     openRPCPort = true;
     settings = {
       download-dir = "/media/nas/downloads";
+      incomplete-dir = "/media/nas/downloads/.incomplete";
       rpc-whitelist = "127.0.0.1,10.0.0.1,100.103.94.88,100.91.137.34,100.82.251.87,100.105.196.3";
       rpc-bind-address = "0.0.0.0"; # Bind to own IP
     };
