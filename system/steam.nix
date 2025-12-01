bee8f9a55ecf839ff5ca1dc8d15bf8b9d3411f16
jcw <github.u9l99@aleeas.com>
2025-12-01 00:44:42 -0700

    added steam remoteplay

--- system/steam.nix
+++ system/steam.nix
@@ -7,6 +7,8 @@
 {
   programs.steam = {
     enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    localNetworkGameTransfers.openFirewall = true;
     gamescopeSession = {
       enable = true;
     };
