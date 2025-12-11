c7a484da96e27c8bf113fb18edccdc05b739cee0
jcw <github.u9l99@aleeas.com>
2025-12-11 14:22:38 -0700

    maybe fixed nfs

--- system/nfs-client.nix
+++ system/nfs-client.nix
@@ -3,9 +3,10 @@
   fileSystems."/media/shared" = {
     device = "100.85.215.105:/shared";
     fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto"  ];
+    options = [ "x-systemd.automount" "nfsvers=4.2" "noauto"  ];
   }; 
   boot.supportedFilesystems = [ "nfs" ];
+  boot.kernelModules = [ "nfs" ];
   boot.initrd = {
     supportedFilesystems = [ "nfs" ];
     kernelModules = [ "nfs" ];
