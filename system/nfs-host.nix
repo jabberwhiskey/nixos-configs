3328c3ab265805011bd96d7b74d663e0237eaf72
jcw <github.u9l99@aleeas.com>
2025-12-11 15:11:54 -0700

    fixed nfs, added other hosts

--- system/nfs-host.nix
+++ system/nfs-host.nix
@@ -9,7 +9,7 @@
     hostName = "100.85.215.105";
     exports = ''
       /export  100.105.196.3(rw,fsid=0,no_subtree_check)
-      /export/shared  100.105.196.3(rw,sync,nohide,insecure,no_subtree_check) 192.168.68.0/24(rw,sync,nohide,insecure,no_subtree_check)
+      /export/shared  100.105.196.3(rw,sync,nohide,insecure,no_subtree_check) 192.168.68.0/24(rw,sync,nohide,insecure,no_subtree_check) 100.103.94.88(rw,sync,nohide,insecure,no_subtree_check) 100.99.171.95(rw,sync,nohide,insecure,no_subtree_check)
     '';
     lockdPort = 4001;
     mountdPort = 4002;
