7ea070b77999b01047e0ac91d71e0d045030a8a1
jcw <github.u9l99@aleeas.com>
2025-12-14 02:09:58 -0700

    maybe fixed nfs

--- system/nfs-host.nix
+++ system/nfs-host.nix
@@ -14,6 +14,9 @@
     lockdPort = 4001;
     mountdPort = 4002;
     statdPort = 4000; 
+    extraNfsdConfig = ''
+      host=100.85.215.105
+    '';
   };
   networking.firewall = {
     allowedTCPPorts = [ 111  2049 4000 4001 4002 20048 ];
