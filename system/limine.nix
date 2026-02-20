73132d4c426f0adbe86f883edc221d8c32d7ea5e
jcw <github.u9l99@aleeas.com>
2026-02-20 06:33:22 -0700

    added limine

--- system/limine.nix
+++ system/limine.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, config, ...}:
 {
   boot.loader = {
-    systemd.enable = lib.mkForce false;
+    systemd-boot.enable = lib.mkForce false;
     limine = {
       enable = true;
       secureBoot.enable = true;
