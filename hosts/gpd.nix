777f817107a8af45b42d10ffe6324b2a17c382b9
jcw <github.u9l99@aleeas.com>
2026-03-12 22:16:53 -0600

    remove steamdeck specific config

--- hosts/gpd.nix
+++ hosts/gpd.nix
@@ -41,9 +41,6 @@
   #  specialArgs = { inherit inputs; };
   system.stateVersion = "25.05";
   jovian = {
-    devices.steamdeck.enable = true;
-    devices.steamdeck.enableKernelPatches = true;
-    devices.steamdeck.enablePerfControlUdevRules = true;
     steam = {
       user = "jcw";
       autoStart = true;
