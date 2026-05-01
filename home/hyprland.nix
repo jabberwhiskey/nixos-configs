0be6e73d72f1fd8086f22a836dfbf157b0e26dea
jcw <github.u9l99@aleeas.com>
2026-05-01 05:39:29 -0600

    will come back to shikane later

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -6,9 +6,6 @@
   ...
 }:
 {
-  imports = [
-    ./shikane.nix
-  ];
   home = {
     packages = with pkgs; [
       wl-clipboard
--- home/shikane.nix
+++ home/shikane.nix
@@ -2,7 +2,36 @@
 {
   services.shikane = {
     enable = true;
+    settings = {
+      profile = [
+        {
+          name = "laptop-output-only";
+          output = [
+            {
+              search = "eDP-1";
+              enable = true;
+              mode = "1920x1080@60";
+              position = "0,0";
+            }
+          ];
+        }
+        {
+          name = "docked";
+          output = [
+            {
+              search = "eDP-1";
+              enable = false;
+            }
+            {
+              search = ["s=HTKP408161" "m=U28E590" ];
+              enable = true;
+              mode = "3840x2160@59.994700";
+              position = "0,0";
+
+            }
+          ];
+        }
       ];
     };
-  }
+  };
 }
