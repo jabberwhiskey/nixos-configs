afc7c6c9dd01b7abb48c0d4ba2e81fe41492e6e8
jcw <jwillbanks2@gmail.com>
2026-06-11 08:17:52 -0600

    updated

--- system/nfs-host.nix
+++ system/nfs-host.nix
@@ -21,7 +21,8 @@
       lockdPort = 4001;
       mountdPort = 4002;
       statdPort = 4000; 
-    };
+      };
+    };  
   networking.firewall = {
     allowedTCPPorts = [ 111  2049 4000 4001 4002 20048 ];
     allowedUDPPorts = [ 111 2049 4000 4001  4002 20048 ];
